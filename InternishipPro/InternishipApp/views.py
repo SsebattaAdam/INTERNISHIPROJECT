e05740ed85a17f6d9b96c161bb8014ad1d506829
SsebattaAdam <108044666+SsebattaAdam@users.noreply.github.com>
2024-06-11 14:49:27 +0300

    This is the interniship repo

--- InternishipPro/InternishipApp/views.py
+++ InternishipPro/InternishipApp/views.py
@@ -2,12 +2,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 
-def functionCall(request):
-    return HttpResponse("Hello world")
+
 
 def indexPage(request):
     return render(request, 'index.html')
     
 def about(request):
     return render(request, 'about.html')
+
+def service(request):
+    return render(request, 'services.html')
+
+def service_detail(request):
+    return render(request, 'service_details.html')
+
+def get_startednow(request):
+    return render(request, 'get_started.html')
 # Create your views here.
