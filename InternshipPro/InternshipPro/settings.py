a6f00594b35d6f235b6d74d8ef419318eb2e7acc
SsebattaAdam <108044666+SsebattaAdam@users.noreply.github.com>
2024-06-28 15:28:44 +0300

    corrected

--- InternshipPro/InternshipPro/settings.py
+++ InternshipPro/InternshipPro/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Bizconnect',
-    'protected_media.apps.ProtectedMediaConfig',
+   
 ]
 
 MIDDLEWARE = [
--- InternshipPro/InternshipPro/urls.py
+++ InternshipPro/InternshipPro/urls.py
@@ -22,5 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Bizconnect.urls')),
-    path('protected/', include('protected_media.urls')),
+  
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
