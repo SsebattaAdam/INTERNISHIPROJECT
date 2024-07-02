bc09c90ec62067589dbf5eb23ca50c1d099f2cf0
Hadijah Lule Nalukwago <108068324+hatimah@users.noreply.github.com>
2024-07-02 12:30:02 +0300

    more work

--- InternshipPro/Bizconnect/models.py
+++ InternshipPro/Bizconnect/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-
 # Create your models here.
 from django.db import models
 from django.contrib.auth.models import User
@@ -50,7 +49,7 @@ class ExpertRegistration(models.Model):
         ('tanzania', 'Tanzania'),
         ('kenya', 'Kenya'),
     ]
-
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default=1)
     surname = models.CharField(max_length=255)
     firstname = models.CharField(max_length=255)
     gender = models.CharField(max_length=6, choices=GENDER_CHOICES)
@@ -117,6 +116,68 @@ class InvestmentDeal(models.Model):
     valuation = models.DecimalField(max_digits=10, decimal_places=2)
     terms = models.TextField()
     entreprenuer = models.ForeignKey(Registration, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+    
+
+
+class ConsultationPackage(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    package_type = models.CharField(max_length=20, choices=[
+        ('hourly', 'Hourly Rate'),
+        ('retainer', 'Retainer-based'),
+        ('project', 'Project-Based'),
+        ('specialised', 'Specialised Challenge'),
+        ('growth', 'Growth Strategy'),
+    ])
+    package_price = models.DecimalField(max_digits=10, decimal_places=2)
+    expert = models.ForeignKey(ExpertRegistration, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+    
+
+
+
+    
+
+class ScheduledMeeting(models.Model):
+    STATUS_CHOICES = [
+        ('Pending', 'Pending'),
+        ('Approved', 'Approved'),
+        ('Denied', 'Denied')
+    ]
+    
+    title = models.CharField(max_length=255)
+    expert_name = models.CharField(max_length=255)
+    consultation_date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    link = models.URLField()
+    consultation_package = models.ForeignKey(ConsultationPackage, on_delete=models.CASCADE)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='Pending')
+    denial_reason = models.TextField(blank=True, null=True)  # Field for the reason of denial
+    
+    def __str__(self):
+        return f"{self.title} with {self.expert_name} on {self.consultation_date}"
+    
+class InvestmentFunds(models.Model):
+    title = models.CharField(max_length=200)
+    industry = models.CharField(max_length=100)
+    type = models.CharField(max_length=100, choices=[
+        ('equity', 'Equity'),
+        ('debt', 'Debt'),
+        ('convertible_note', 'Conertible Note'),
+    ])
+    investment_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    contact_method = models.CharField(max_length=100, choices=[
+        ('email', 'Email'),
+        ('phone', 'Phone'),
+    ])
+    notes = models.TextField()
+    supporting_documents = models.FileField(upload_to='supporting_documents/', null=True, blank=True)
 
     def __str__(self):
         return self.title
