a871a4ea6ae22ed3f0e2d2cb6b0c42097f8fd0a8
Hadijah Lule Nalukwago <108068324+hatimah@users.noreply.github.com>
2024-07-04 13:18:24 +0300

    update

--- InternshipPro/Bizconnect/urls.py
+++ InternshipPro/Bizconnect/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
   path("entreprenuer/requests/form/", views.service_request_form, name="expert_request_form"),
   path("entreprenuer/schedule/", views.consultation_schedule, name="consultation_schedule"),
   path("entreprenuer/schedule/form/", views.consultation_schedule_form, name="consultation_schedule_form"),
-  path("entreprenuer/investment/deals/", views.investment_deal, name="investment_deals"),
+  path("entreprenuer/investment/deals/", views.investment_deals, name="investment_deals"),
   path("entreprenuer/investment/deals/form/", views.investment_deal_form, name="investment_deal_form"),
   
   ## These are the Investors' urls
