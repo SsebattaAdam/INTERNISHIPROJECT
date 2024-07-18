093326231fd8c0327e46db0f4d8cb433d4913dfc
andrewkah <107668588+andrewkah@users.noreply.github.com>
2024-07-18 22:49:49 +0300

    Final commit
    This is the complete business end, containing the templates and their views.

--- InternshipPro/Bizconnect/models.py
+++ InternshipPro/Bizconnect/models.py
@@ -18,11 +18,10 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
             'unique': "A user with that email already exists.",
         },)
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
-
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     USERNAME_FIELD = "email"
     EMAIL_FIELD = "email"
@@ -55,6 +54,8 @@ class Registration(models.Model):
     user_type = models.CharField(max_length=20, default='entrepreneur')
     role_in_company = models.CharField(max_length=100, choices=(('founder', 'Founder'), ('owner', 'Owner'), ('director', 'Director')))
     password = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.firstname} {self.surname}"
@@ -99,126 +100,11 @@ class ExpertRegistration(models.Model):
     references = models.TextField()
     user_type = models.CharField(max_length=20, default='expert')
     password = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.firstname} {self.surname}"
-
-
-
-class ServiceRequest(models.Model):
-    business_idea = models.CharField(max_length=255)
-    industry = models.CharField(max_length=100)
-    description = models.TextField()
-    target_market = models.TextField()
-    consultation_time = models.TimeField()
-    consultation_date = models.DateField()
-    urgency_level = models.CharField(max_length=10)
-    attachment = models.FileField(upload_to='attachments/', blank=True, null=True)
-    comments = models.TextField(blank=True, null=True)
-    submitted_at = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=20, choices=[('Pending', 'Pending'), ('Completed', 'Completed')], default='Pending')
-    assigned_expert = models.ForeignKey('ExpertRegistration', on_delete=models.SET_NULL, null=True, blank=True, related_name='assigned_requests',)
-    requester = models.ForeignKey(Registration, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f"{self.business_idea} by {self.requester.firstname} {self.requester.surname}"
-
-
-
-
-class BusinessIdeas(models.Model):
-    entrepreneur = models.ForeignKey(Registration, related_name='business_ideas', on_delete=models.CASCADE)
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    industry = models.CharField(max_length=100)
-    target_market = models.TextField()
-    business_model = models.TextField()
-    projections = models.TextField()
-    goals = models.TextField(blank=True, null=True)
-    pitch_deck = models.FileField(upload_to='attachments/pitch_deck/', blank=True, null=True)
-    plan = models.FileField(upload_to='attachments/plan/', blank=True, null=True)
-    video = models.FileField(upload_to='attachments/video/', blank=True, null=True)
-    support = models.FileField(upload_to='attachments/support/', blank=True, null=True)
-    submitted_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.title
-
-
-class InvestmentDeal(models.Model):
-    entrepreneur = models.ForeignKey(Registration, related_name='investment_deals', on_delete=models.CASCADE)
-    title = models.CharField(max_length=255)
-    industry = models.CharField(max_length=255)
-    funding_goal = models.DecimalField(max_digits=10, decimal_places=2)
-    valuation = models.DecimalField(max_digits=10, decimal_places=2)
-    terms = models.TextField()
-
-    def __str__(self):
-        return self.title
-    
-
-
-class ConsultationPackage(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    package_type = models.CharField(max_length=50, choices=[
-        ('Hourly Rate', 'Hourly Rate'),
-        ('Retainer-based', 'Retainer-based'),
-        ('Project-Based', 'Project-Based'),
-        ('Specialised Challenge', 'Specialised Challenge'),
-        ('Growth Strategy', 'Growth Strategy'),
-    ])
-    package_price = models.DecimalField(max_digits=10, decimal_places=2)
-    expert = models.ForeignKey(ExpertRegistration, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.title
-    
-
-
-
-    
-
-class ScheduledMeeting(models.Model):
-    STATUS_CHOICES = [
-        ('Pending', 'Pending'),
-        ('Approved', 'Approved'),
-        ('Denied', 'Denied')
-    ]
-    entrepreneur = models.ForeignKey(Registration, related_name='schedule_meeting', on_delete=models.CASCADE)
-    title = models.CharField(max_length=255)
-    expert_name = models.CharField(max_length=255)
-    consultation_date = models.DateField()
-    start_time = models.TimeField()
-    end_time = models.TimeField()
-    link = models.URLField()
-    consultation_package = models.ForeignKey(ConsultationPackage, on_delete=models.CASCADE)
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='Pending')
-    denial_reason = models.TextField(blank=True, null=True)  # Field for the reason of denial
-    
-    def __str__(self):
-        return f"{self.title} with {self.expert_name} on {self.consultation_date}"
-    
-class InvestmentFunds(models.Model):
-    entrepreneur = models.ForeignKey(Registration, related_name='investment_fund', on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    industry = models.CharField(max_length=100)
-    type = models.CharField(max_length=100, choices=[
-        ('Equity', 'Equity'),
-        ('Debt', 'Debt'),
-        ('Convertible Note', 'Convertible Note'),
-    ])
-    investment_amount = models.DecimalField(max_digits=10, decimal_places=2)
-    contact_method = models.CharField(max_length=100, choices=[
-        ('Email', 'Email'),
-        ('Phone', 'Phone'),
-    ])
-    notes = models.TextField()
-    supporting_documents = models.FileField(upload_to='supporting_documents/', null=True, blank=True)
-
-    def __str__(self):
-        return self.title
-
 
 class Investor(models.Model):
     TYPE_CHOICES = [
@@ -254,17 +140,162 @@ class Investor(models.Model):
     health = models.BooleanField(default=False)
     wholesale = models.BooleanField(default=False)
     user_type = models.CharField(max_length=20, default='investor')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.type} - {self.email}"
 
+class ServiceRequest(models.Model):
+    business_idea = models.CharField(max_length=255)
+    industry = models.CharField(max_length=100)
+    description = models.TextField()
+    target_market = models.TextField()
+    consultation_time = models.TimeField()
+    consultation_date = models.DateField()
+    urgency_level = models.CharField(max_length=10)
+    attachment = models.FileField(upload_to='attachments/', blank=True, null=True)
+    comments = models.TextField(blank=True, null=True)
+    submitted_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20, choices=[('Pending', 'Pending'), ('Completed', 'Completed'), ('Scheduled', 'Scheduled'), ('Concluded','Concluded')], default='Pending')
+    assigned_expert = models.ForeignKey(ExpertRegistration, on_delete=models.SET_NULL, null=True, blank=True, related_name='assigned_requests',)
+    requester = models.ForeignKey(Registration, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f"{self.business_idea} by {self.requester.firstname} {self.requester.surname}"
+
+
+
+
+class BusinessIdeas(models.Model):
+    entrepreneur = models.ForeignKey(Registration, related_name='business_ideas', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    industry = models.CharField(max_length=100)
+    target_market = models.TextField()
+    business_model = models.TextField()
+    projections = models.TextField()
+    goals = models.TextField(blank=True, null=True)
+    pitch_deck = models.FileField(upload_to='attachments/pitch_deck/', blank=True, null=True)
+    plan = models.FileField(upload_to='attachments/plan/', blank=True, null=True)
+    video = models.FileField(upload_to='attachments/video/', blank=True, null=True)
+    support = models.FileField(upload_to='attachments/support/', blank=True, null=True)
+    submitted_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class InvestmentDeal(models.Model):
+    entrepreneur = models.ForeignKey(Registration, related_name='investment_deals', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    industry = models.CharField(max_length=255)
+    funding_goal = models.DecimalField(max_digits=10, decimal_places=2)
+    valuation = models.DecimalField(max_digits=10, decimal_places=2)
+    terms = models.TextField()
+    status = models.CharField(max_length=20, choices=[('Pending', 'Pending'), ('Funded', 'Funded')], default='Pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+
+
+class ConsultationPackage(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    package_type = models.CharField(max_length=50, choices=[
+        ('Hourly Rate', 'Hourly Rate'),
+        ('Retainer-based', 'Retainer-based'),
+        ('Project-Based', 'Project-Based'),
+        ('Specialised Challenge', 'Specialised Challenge'),
+        ('Growth Strategy', 'Growth Strategy'),
+    ])
+    package_price = models.DecimalField(max_digits=10, decimal_places=2)
+    expert = models.ForeignKey(ExpertRegistration, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+
+
+
+    
+
+class ScheduledMeeting(models.Model):
+    STATUS_CHOICES = [
+        ('Pending', 'Pending'),
+        ('Approved', 'Approved'),
+        ('Denied', 'Denied')
+    ]
+    service_request = models.ForeignKey(ServiceRequest, on_delete=models.CASCADE, related_name='schedule_meeting')
+    entrepreneur = models.ForeignKey(Registration, related_name='schedule_meeting', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    expert_name = models.CharField(max_length=255)
+    consultation_date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    link = models.URLField()
+    consultation_package = models.ForeignKey(ConsultationPackage, on_delete=models.CASCADE)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='Pending')
+    denial_reason = models.TextField(blank=True, null=True)  # Field for the reason of denial
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"{self.title} with {self.expert_name} on {self.consultation_date}"
+    
+    
+class InvestmentFunds(models.Model):
+    investment_deal = models.ForeignKey(InvestmentDeal, on_delete=models.CASCADE, related_name='investment_fund')
+    title = models.CharField(max_length=200)
+    industry = models.CharField(max_length=100)
+    type = models.CharField(max_length=100, choices=[
+        ('Equity', 'Equity'),
+        ('Debt', 'Debt'),
+        ('Convertible Note', 'Convertible Note'),
+    ])
+    investment_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    contact_method = models.CharField(max_length=100, choices=[
+        ('Email', 'Email'),
+        ('Phone', 'Phone'),
+    ])
+    notes = models.TextField()
+    supporting_documents = models.FileField(upload_to='supporting_documents/', null=True, blank=True)
+    investor = models.ForeignKey(Investor,  on_delete=models.CASCADE, related_name='investment_fund')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Resource(models.Model):
+    expert = models.ForeignKey(ExpertRegistration, on_delete=models.CASCADE, related_name='resource')
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+    type = models.CharField(max_length=100, choices=[
+        ('Article', 'Article'),
+        ('Template', 'Template'),
+        ('Practice', 'Best Practice'),
+    ])
+    supporting_documents = models.FileField(upload_to='supporting_documents/', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 class ReplyRequest(models.Model):
-    meeting = models.ForeignKey('ScheduledMeeting', on_delete=models.CASCADE)
+    meeting = models.ForeignKey(ScheduledMeeting, on_delete=models.CASCADE)
+    service_request = models.ForeignKey(ServiceRequest, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    description = models.TextField()
-    text_area = models.TextField()
+    resource = models.ForeignKey(Resource, on_delete=models.CASCADE)
+    description = models.TextField(max_length=500)
+    comments = models.TextField(max_length=500)
     status_choices = [
         ('SENT', 'Sent'),
         ('NOT_SENT', 'Not Sent'),
--- InternshipPro/Bizconnect/urls.py
+++ InternshipPro/Bizconnect/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
   path("membership/", views.after_register, name="after_register"),
   
   ## These are the entrepreneurs' urls
-  path('entrepreneurs/', views.homepage1, name = 'homepage1'),
+  path('dashboard/', views.homepage1, name = 'homepage1'),
   path('entrepreneur/ideals/', views.business_ideals, name = 'business_ideals'),
   path("entrepreneur/ideal/form/", views.business_ideal_form , name="business_ideal_form"),
   path("entrepreneur/requests/", views.service_requests, name="service_requests"),
@@ -28,13 +28,12 @@ urlpatterns = [
   path("entrepreneur/investment/deals/form/", views.investment_deal_form, name="investment_deal_form"),
   
   ## These are the Investors' urls
-  path('investors/', views.investorhomepage, name = 'investorhomepage'),
   path("investor/fundings/", views.investment_fundings, name="investment_fundings"),
-  path("investor/funding/form/", views.investment_funding_form, name="investment_funding_form"),
+  path("investor/funding/form/<int:deal_id>/", views.investment_funding_form, name="investment_funding_form"),
   path('submit-investor/', views.submit_investor_form, name='submit_investor_form'),
-  path('submit-investor_deals/', views.investor_deals, name='investordeals'),
+  path('investors/investor_deals/', views.investor_deals, name='investor_deals'),
+  path('business_idea/<int:idea_id>/', views.businessidea_detail, name='businessidea_detail'),
   ## These are the Experts' urls
-  path('experts/', views.experthomepage, name = 'experthomepage'),
   path('expert/library/', views.resources, name = 'resources'),
   path("expert/library/form/", views.resource_form, name="resource_form"),
   path("expert/requests/", views.assistance_request, name="assistance_request"),
@@ -61,21 +60,18 @@ urlpatterns = [
 
   #admin urls
   path('tables/', views.allTables, name='allTables'),
-  path('list_requestsmade/', views.list_requestsmade, name='list_requestsmade1'),
-
   path('loginAdmin/', views.loginAdmin, name='loginAdmin'),
-  path('logout/', views.logout2, name='logout2'),
+  path('logoutAdmin/', views.logout2, name='logout2'),
   path('admin2/', views.admin2, name='admin2'),
   path('approve_request/<int:request_id>/', views.approve_request, name='approve_request'),
   path('create_package/', views.create_consultation_package, name='create_package'),
 
-  path('schedule_meeting/', views.schedule_meeting4theent, name='schedule_meeting'),
+  path('schedule_meeting/<int:request_id>/', views.schedule_meeting4theent, name='schedule_meeting'),
 
 
   path('update-meeting-status/<int:meeting_id>/<str:status>/', views.update_meeting_status, name='update_meeting_status'),
 
 
-  path('investment_dealists/', views.investment_deallists, name='investment_dealists'),
   path('reply-requests/', views.replay_requests_made, name='replay_requests'),
   path('reply_madenow/', views.replay_requests_madetothemeeting, name='replay_requests_madetothemeeting'),
 
